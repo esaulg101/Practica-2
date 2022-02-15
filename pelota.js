c4ce25f5df6d6e3e6e91fd91f5a3a206379f3015
esaulg101 <99497187+esaulg101@users.noreply.github.com>
2022-02-15 15:01:20 -0600

    Add files via upload

---
+++ pelota.js
@@ -0,0 +1,11 @@
+class pelota{
+    constructor(){
+        this.x=50;
+        this.y=100;
+        this.width=20;
+        this.heigth=20;
+    }
+    display(){
+        circle( this.x, this.y, this.width, this.heigth);
+    }
+}
